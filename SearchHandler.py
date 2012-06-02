04402a96fa8d282f25993b202a0884e69661526e
susan wang <susanwang1015@gmail.com>
2012-06-02 14:05:04 -0500

    Revert "tornado template"
    This reverts commit 0dd7379c73904482a4ba1cee94dbbf6ca4911b0d.

--- SearchHandler.py
+++
@@ -1,9 +0,0 @@
-import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import tornado.web
-
-class SearchHandler(tornado.web.RequestHandler):
-	@tornado.web.asynchronous
-	def get(self):
-		self.render('index.html')
