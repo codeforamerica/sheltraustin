0dd7379c73904482a4ba1cee94dbbf6ca4911b0d
Efe Karakus <efekarakus@gmail.com>
2012-06-02 14:02:08 -0500

    tornado template

---
+++ server.py
@@ -0,0 +1,33 @@
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import os.path
+
+from SearchHandler import SearchHandler
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", SearchHandler)
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            handler_path=os.path.join(os.path.dirname(__file__), "handlers"),
+            xsrf_cookies=True,
+            autoescape="xhtml_escape",
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+def main():
+    tornado.options.parse_command_line()
+    app = Application()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
