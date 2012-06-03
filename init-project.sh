39a88e9996f6cce8dae891a33963e4fcdde89367
Joe Merante <merante.joe@gmail.com>
2012-06-03 10:53:37 -0500

    commit after init files created before heroku

--- init-project.sh
+++ init-project.sh
@@ -1,3 +1,17 @@
+# Project creation script
+# (for Tornado on Heroku)
+#
+# by Mike Dory | dory.me
+# 11.12.11
+# modified by Tedb0t | tedbot.com
+
+# --------------------------
+
+# git!
+git init
+
+# --------------------------
+
 # set up the pip requirements
 touch requirements.txt
 echo "Tornado==2.1.1" >> requirements.txt
@@ -6,5 +20,16 @@ echo "Tornado==2.1.1" >> requirements.txt
 touch Procfile
 echo "web: python main.py" >> Procfile
 
+# --------------------------
+
+echo "Committing to Git"
+
+git add .
+git commit -m "Create heroku"
+
+echo "Creating Heroku app & pushing"
+
 heroku create --stack cedar
 git push heroku master
+
+echo "All Done!"
