24a5dc719860f7f493ea6739ca5e6a696facc302
Joe Merante <merante.joe@gmail.com>
2012-06-03 11:09:50 -0500

    change default http

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from AutocompleteHandler import AutocompleteHandler
 from InfoHandler import InfoHandler
 from tornado.options import define, options
 
-define("port", default=8888, help="run on the given port", type=int)
+define("port", default=80, help="run on the given port", type=int)
 
 class Application(tornado.web.Application):
     def __init__(self):
