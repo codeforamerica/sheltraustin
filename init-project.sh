096fd8497d7dd7ffd56eeb7a2845d3a724b0d0e5
Joe Merante <merante.joe@gmail.com>
2012-06-03 10:28:52 -0500

    changed some names for heroku

--- init-project.sh
+++ init-project.sh
@@ -5,3 +5,6 @@ echo "Tornado==2.1.1" >> requirements.txt
 # set up the Procfile
 touch Procfile
 echo "web: python main.py" >> Procfile
+
+heroku create --stack cedar
+git push heroku master
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import os.path
+import os
+import tornado.escape
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-import os.path
+import unicodedata
+
 
 from SearchHandler import SearchHandler
 from AutocompleteHandler import AutocompleteHandler
@@ -14,7 +18,7 @@ define("port", default=8888, help="run on the given port", type=int)
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/", DefaultHandler),
+            (r"/", MainHandler),
             (r"/find*",AutocompleteHandler),
             (r"/search*", SearchHandler),
             (r"/info*", InfoHandler)
@@ -29,7 +33,7 @@ class Application(tornado.web.Application):
         tornado.web.Application.__init__(self, handlers, **settings)
 
 
-class DefaultHandler(tornado.web.RequestHandler):
+class MainHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         self.render('index.html')
@@ -39,6 +43,8 @@ def main():
     app = Application()
     app.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(os.environ.get("PORT", 8888))
 
 if __name__ == "__main__":
     main()
